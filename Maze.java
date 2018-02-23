5a8d4ffcef62f843526285e3ea8539fc19b278b4
Danny Huynh <danhuynh0803@gmail.com>
2018-02-23 18:26:02 -0500

    Refactor node and move AStar to Maze.java

--- Maze.java
+++ Maze.java
@@ -1,12 +1,13 @@
+import java.util.*;
 
-public class Maze {
+ public class Maze {
     public int length;
     public int width;
     public Node Start;
     public Node End;
     public Node[][] board;
 
-    public Maze(int length,int width,double p){
+    public Maze(int length, int width, double p){
         this.length = length;
         this.width = width;
         this.board = new Node[length][width];
@@ -14,14 +15,12 @@ public class Maze {
         for(int i=0;i< length;i++){
             for(int j=0;j < width;j++){
                 this.board[i][j] = new Node();
-
                 // Set the node as being a wall
                 // depending on the probability 'p' that is passed in
                 if (Math.random() < p)
                 {
                     this.board[i][j].isBlocked = true;
                 }
-       
             }
         }
 
@@ -39,22 +38,99 @@ public class Maze {
             for(int j=0;j< width;j++){
                 // Set West
                 if( i>0 && !this.board[i-1][j].isBlocked){
-                    this.board[i][j].west = this.board[i-1][j];
+                    this.board[i][j].neighbors.add(this.board[i-1][j]);
                 }
                 // Set East
                 if( i< this.length-1 && !this.board[i+1][j].isBlocked){
-                    this.board[i][j].east = this.board[i+1][j];
+                    this.board[i][j].neighbors.add(this.board[i+1][j]);
                 }
                 // Set South
                 if( j< this.width-1 && !this.board[i][j+1].isBlocked){
-                    this.board[i][j].south = this.board[i][j+1];
+                    this.board[i][j].neighbors.add(this.board[i][j+1]);
                 }
                 // Set North
                 if (j > 0 && !this.board[i][j-1].isBlocked){
-                    this.board[i][j].north = this.board[i][j-1];
+                    this.board[i][j].neighbors.add(this.board[i][j-1]);
                 }
             }
         }
+    }
+
+    //checks to see if current node is in range of grid
+    public boolean isValid(int row, int col)
+    {
+        return (row >=0) && (row < length) && (col >= 0) && (col < width);
+    }
+
+    public boolean isBlocked(int row, int col)
+    {
+        if(board[row][col].isBlocked)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean isDestination(int row, int col)
+    {
+        if(board[row][col] == End)
+            return true;
+        else   
+            return false;
+    }
+
+    public double calculateHManhattan(int row, int col)
+    {
+        return Math.abs(row - (length-1)) + Math.abs(col - (width-1));
+    }
+
+    public double calculateHValue(int row, int col)
+    {
+        return ((double) Math.sqrt((row-(length-1)*(row-(length-1)) + (col-(width-1)*(col-(width-1))))));
+    }
+
+    public void tracePath()
+    {
+        
+    }
+
+    public void aStarSearch()
+    {
+        Set<Node> openSet = new LinkedHashSet();
+        Set<Node> closedSet = new LinkedHashSet();
+
+        // Add the Starting node to the openset
+        Node q;
+        openSet.add(Start);
+
+        while(!openSet.isEmpty())
+        {
+            q = getLowestNode(openSet);
+        }
+
+
+    }
+
+    // Returns the node with the lowest 'f' within the open set
+    private Node getLowestNode(Set<Node> openSet)
+    {
+        // find the node with the least f
+        double minF = Double.MAX_VALUE;
+        Node[] openArray = openSet.toArray(new Node[0]);
+
+        Node q = null;
+        for (int i = 0; i < openArray.length; ++i)
+        {
+            if (openArray[i].f < minF)    
+            {
+                minF = openArray[i].f;
+                q = openArray[i];
+            }
+        }
+        return q;
     }
 
     public void print(){
@@ -85,23 +161,6 @@ public class Maze {
     // adjecent connections for a particular node
     static int displayConnections(Node node)
     {
-        int count = 0;
-        if (node.north != null)
-        {
-            count++;
-        }
-        if (node.south != null)
-        {
-            count++;
-        }
-        if (node.east != null)
-        {
-            count++;
-        }
-        if (node.west != null)
-        {
-            count++;
-        }
-        return count;
+        return node.neighbors.size();
     }
 }
--- Node.java
+++ Node.java
@@ -3,19 +3,21 @@ import java.util.*;
 public class Node {
     boolean isBlocked;
     boolean isVisited;
-    Node north;
-    Node south;
-    Node east;
-    Node west;
-    public int x;
-    public in y;
+    // Maximum of four neighbors
+    List<Node> neighbors = new ArrayList<Node>();
+    // The prior node that we came from in our path
+    // Used for calculating 'f' 
+    Node parent;
+    // Heuristic for A* search
+    double f, g, h;
 
     public Node() {
-        this.north = null;
-        this.south = null;
-        this.east = null;
-        this.west = null;
         this.isBlocked = false;
         this.isVisited = false;
+
+        this.parent = null;
+        f = Double.MAX_VALUE;
+        g = Double.MAX_VALUE;
+        h = Double.MAX_VALUE;
     }
 }
