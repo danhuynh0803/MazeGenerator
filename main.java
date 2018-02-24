eaaf8bc343df3e81873da9159d6151785ade0644
Danny Huynh <danhuynh0803@gmail.com>
2018-02-23 20:31:16 -0500

    remove extra print

--- main.java
+++ main.java
@@ -2,9 +2,7 @@
 public class main {
     public static void main(String[] args) {
         Maze b = new Maze(50,50,0.3);
-        b.print();
         b.aStarSearch();
-        System.out.println("Printing path");
         b.print();
     }
 }
